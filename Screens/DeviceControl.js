ec40d9289c5570151804891c3be40a47b8fe2906
sardaharshmoonpreneur <hs@moonpreneur.com>
2025-03-31 01:42:53 +0530

    "Session 38"

--- Screens/DeviceControl.js
+++ Screens/DeviceControl.js
@@ -16,6 +16,124 @@ const DeviceControl = ({ navigation, route }) => {
     const [selectedType, setSelectedType] = useState('');
     const [componentStatuses, setComponentStatuses] = useState({});
 
+    useEffect(() => {
+        if (isFocused) {
+            fetchToken();
+        }
+    }, [isFocused]);
+
+    const fetchToken = async () => {
+        try {
+            const storedLoginResponse = await AsyncStorage.getItem('loginResponse');
+            if (storedLoginResponse) {
+                const parsedResponse = JSON.parse(storedLoginResponse);
+                const newToken = parsedResponse.result[0].token;
+                setToken(newToken);
+                fetchComponents(newToken);
+            }
+        } catch (error) {
+            console.error('Error retrieving token:', error);
+        }
+    };
+
+    const fetchUserProfile = async (token) => {
+        if (!token) return;
+
+        try {
+            const response = await fetch('https://test.moonr.com/LMSService/api/IOT/UserProfile', {
+                method: 'GET',
+                headers: { 'Authorization': `Bearer ${token}` },
+            });
+
+            if (response.status === 200) {
+                const data = await response.json();
+                const userProfile = data?.result?.[0];
+                setSecurityToken(userProfile?.security_token || 'N/A');
+                // console.log('security token here : ', securityToken);
+            } else {
+                console.error('Failed to fetch profile:', response.status);
+            }
+        } catch (error) {
+            console.error('Error fetching profile:', error);
+        }
+    };
+
+    const saveComponent = async () => {
+        if (!componentName || !selectedType) {
+            Alert.alert('Error', 'Please provide a component name and type');
+            return;
+        }
+        console.log("Component Name:", componentName);
+        console.log("Selecrted type Name:", selectedType);
+
+        setIsLoading(true);
+
+        let finalComponentPin = '';
+
+
+        if (selectedType === 'Sensor') {
+            finalComponentPin = 'Senso';
+        } else if (selectedType === 'Toggle') {
+            finalComponentPin = 'Toggle';
+        } else {
+            Alert.alert('Error', 'Invalid component type selected');
+            setIsLoading(false);
+            return;
+        }
+        let component_id
+        if (selectedType === 'Sensor') {
+            component_id = 5;
+        } else {
+            component_id = 1;
+        }
+        console.log("Final Component Pin:", finalComponentPin);
+
+        const componentData = {
+            user_device_id: device.user_device_id,
+            device_component_id: 0,
+            device_component_name: componentName,
+            component_id: component_id,
+            component_pin: finalComponentPin,
+            is_active: 1,
+        };
+        console.log("Request data:", componentData);
+
+        try {
+            const response = await fetch('https://test.moonr.com/LMSService/api/IOT/AddUpdateComponent', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'security-key': securityToken,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(componentData),
+            });
+
+            const result = await response.json();
+
+            if (result.isError) {
+                const errorMessages = result.responseException.validationErrors
+                    .map(error => error.errorMessage)
+                    .join('\n');
+                Alert.alert('Validation Error', errorMessages);
+            }
+
+            if (response.ok && result.statusCode === 200) {
+                Alert.alert('Success', 'Component saved successfully');
+                // fetchComponents(token);
+                setBottomSheetVisible(false);
+            } else {
+                Alert.alert('Error', result.message || 'Failed to save component');
+            }
+        } catch (error) {
+            console.error('Error saving component:', error);
+            Alert.alert('Error', 'Something went wrong');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+
     return (
         <ImageBackground source={require('../images/chs.png')} style={{ width: '100%', height: '100%' }}>
             <View style={{ flex: 1, }}>
@@ -75,22 +193,23 @@ const DeviceControl = ({ navigation, route }) => {
                                     style={styles.input}
                                     placeholder="Component Name"
                                     value={componentName}
+                                    onChangeText={setComponentName}
                                 />
                                 <TouchableOpacity
                                     style={[styles.optionButton, selectedType === 'Sensor' && styles.selectedOption]}
-                                >
+                                    onPress={() => setSelectedType('Sensor')}>
                                     <Text style={styles.optionText}>Sensor</Text>
                                 </TouchableOpacity>
                                 <TouchableOpacity
                                     style={[styles.optionButton, selectedType === 'Toggle' && styles.selectedOption]}
-                                >
+                                    onPress={() => setSelectedType('Toggle')}>
                                     <Text style={styles.optionText}>Toggle</Text>
                                 </TouchableOpacity>
                                 <TouchableOpacity style={styles.cancelButton} onPress={() => setBottomSheetVisible(false)}>
                                     <Text style={styles.cancelButtonText}>Cancel</Text>
                                 </TouchableOpacity>
 
-                                <TouchableOpacity style={styles.save} disabled={isLoading}>
+                                <TouchableOpacity style={styles.save} onPress={saveComponent} disabled={isLoading}>
                                     {isLoading ? <ActivityIndicator size="small" color="#fff" /> : <Text style={styles.saveText}>Save</Text>}
                                 </TouchableOpacity>
                             </View>
@@ -108,8 +227,10 @@ const styles = StyleSheet.create({
     save: { backgroundColor: '#EC8588', padding: 12, borderRadius: 15, alignItems: 'center', marginTop: 10 },
     buttonText: { color: '#EC8588', fontSize: 16, fontWeight: 'bold' },
     saveText: { color: 'white', fontSize: 16, fontWeight: 'bold' },
-    componentItem: { flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: 8, backgroundColor: 'white', 
-        borderRadius: 20, marginBottom: 10 },
+    componentItem: {
+        flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: 8, backgroundColor: 'white',
+        borderRadius: 20, marginBottom: 10
+    },
     bottomSheetContainer: { flex: 1, justifyContent: 'flex-end', backgroundColor: 'rgba(0,0,0,0.5)' },
     bottomSheet: { backgroundColor: '#fff', padding: 20, borderTopLeftRadius: 15, borderTopRightRadius: 15 },
     bottomSheetTitle: { fontSize: 18, fontWeight: 'bold', marginBottom: 10 },
