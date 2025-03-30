1d2d190f804b3f0f475f8720527635a3be5ad07a
sardaharshmoonpreneur <hs@moonpreneur.com>
2025-03-31 00:49:46 +0530

    "Session 35"

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './Screens/LoginScreen';
 import DrawerScreen from './Navigation/Drawer';
 import AddDevice from './Screens/AddDevice';
-
+import DeviceDetail from './Screens/DeviceDetail';
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -15,6 +15,10 @@ export default function App() {
         <Stack.Screen name="LogIn" component={LoginScreen} options={{title: "LogIn", headerShown: false,}}></Stack.Screen>
         <Stack.Screen name="Drawer" component={DrawerScreen} options={{ headerShown: false }}></Stack.Screen>
         <Stack.Screen name="AddDevice" component={AddDevice} options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name="DeviceDetail" component={DeviceDetail} options={{
+          title: "DeviceDetail",
+          headerShown: false,
+        }}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ Screens/DeviceDetail.js
@@ -0,0 +1,190 @@
+import React, { useState, useEffect } from "react";
+import { ImageBackground, View, Text, Image, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Clipboard from "expo-clipboard";
+import { Ionicons } from "@expo/vector-icons";
+
+const DeviceDetail = ({ navigation, route }) => {
+    const { screenName, device1 } = route.params;
+
+    const [screen, setScreen] = useState(screenName);
+    const [devicesResponse, setDevicesResponse] = useState([]);
+    const [token, setToken] = useState('');
+
+    const device = screen === 'AddDevice' ? devicesResponse : device1;
+
+    const copyToClipboard = (token) => {
+        Clipboard.setStringAsync(token);
+    };
+
+    return (
+        <ImageBackground source={require('../images/chs.png')} style={styles.imageBackground}>
+            <View style={styles.headerContainer}>
+                <TouchableOpacity onPress={() => navigation.navigate('Drawer', { screen: 'Home' })} style={styles.backButton}>
+                    <Image source={require('../images/a1.png')} style={styles.backIcon} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.titleContainer}>
+                <Text style={styles.deviceTitle}>{device.user_device_name}</Text>
+            </View>
+            <View style={styles.deviceDetailContainer}>
+                <View style={styles.contentContainer}>
+                    <Text style={styles.deviceName}>Device Details</Text>
+
+                    <Text style={styles.connectionType}>{device.connection_type}</Text>
+
+                    <View style={styles.connectionStatusContainer}>
+                        <View style={[styles.statusIndicator, { backgroundColor: device.device_status === "Connected" ? "green" : "red" }]} />
+                        <Text style={styles.connectionType}>{device.device_status}</Text>
+                    </View>
+
+                    <Text style={styles.tokenTitle}>Device Token :</Text>
+                    <View style={styles.copyClipboardView}>
+                        <Text style={styles.deviceTokenText}>{device.device_token}</Text>
+
+                        <TouchableOpacity>
+                            <Ionicons name="copy" size={18} color="#345c74" />
+                        </TouchableOpacity>
+                    </View>
+
+                    <TouchableOpacity style={styles.dynamicButton} >
+                        <Text style={styles.deleteTextStyle}>Delete Device</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.footerContainer}>
+
+                    <TouchableOpacity style={styles.controlScreenIconStyle}>
+                        <Ionicons name="help-outline" size={26} color="white" />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.helpIconStyle}>
+                        <Ionicons name="arrow-forward" size={26} color="white" />
+                    </TouchableOpacity>
+
+                </View>
+            </View>
+        </ImageBackground>
+    );
+};
+
+const styles = StyleSheet.create({
+    imageBackground: {
+        width: "100%",
+        height: "100%"
+    },
+    headerContainer: {
+        flexDirection: "row", width: "100%",
+        paddingHorizontal: 20,
+        marginTop: 20,
+        justifyContent: 'space-between'
+    },
+    backButton: {
+        paddingHorizontal: 10,
+        paddingVertical: 13,
+        borderRadius: 10,
+        marginTop: 30,
+        backgroundColor: "#d1a0a7"
+    },
+    backIcon: {
+        width: 20,
+        height: 15
+    },
+    titleContainer: {
+        width: '100%',
+        height: '20%'
+    },
+    deviceTitle: {
+        color: "white",
+        fontSize: 35,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginTop: 30
+    },
+    deviceDetailContainer: {
+        backgroundColor: 'white',
+        borderRadius: 40,
+        margin: 10
+    },
+    contentContainer: {
+        marginTop: 20,
+        padding: 20,
+        borderRadius: 50,
+        paddingHorizontal: 20
+    },
+    deviceName: {
+        fontSize: 25,
+        fontWeight: "bold",
+        color: '#345c74'
+    },
+    connectionType: {
+        fontSize: 16,
+        fontWeight: "400",
+        color: '#345c74'
+    },
+    connectionStatusContainer: {
+        flexDirection: "row",
+        marginBottom: 10
+    },
+    statusIndicator: {
+        borderRadius: 50,
+        width: 15,
+        height: 15,
+        margin: 5
+    },
+    tokenTitle: {
+        fontSize: 22,
+        fontWeight: "bold",
+        color: '#345c74',
+        marginBottom: 10
+    },
+    copyClipboardView: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#ecf0f1",
+        borderRadius: 10,
+        borderColor: "#5A5A5A",
+        borderWidth: 1,
+        padding: 10
+    },
+    deviceTokenText: {
+        fontSize: 14,
+        fontWeight: "400",
+        color: '#345c74'
+    },
+    dynamicButton: {
+        backgroundColor: "#F18C8E",
+        padding: 10,
+        borderRadius: 15,
+        alignItems: "center",
+        marginTop: 20
+    },
+    deleteTextStyle: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    footerContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        padding: 20
+    },
+    controlScreenIconStyle: {
+        backgroundColor: "#F18C8E",
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        width: 65, height: 65
+    },
+    helpIconStyle: {
+        backgroundColor: "#F18C8E",
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        width: 65,
+        height: 65
+    },
+});
+
+export default DeviceDetail;
